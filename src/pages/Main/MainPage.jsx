68b044a5b8fd5871f1c726ac37cdbcaf61999b0d
Anhagu <leejhsam330@gmail.com>
2023-09-06 16:47:54 +0900

    테스트1

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -38,8 +38,8 @@ function MainPage(props) {
 }
 
 const Background = styled.div`
-    width : 1179px;
-    height : 2556px;
+    width : 100vw;
+    height : 100vh;
     background-color: #ffffff;
 `;
 
@@ -47,12 +47,36 @@ const Header = styled.div`
     display : flex;
     flex-direction : row;
     align-items: center;
-    width : 100%;
-    height: 169px;
+    width : 100vw;
+    height: 8vh;
 
     border-style: solid;
     border-width: 0px 0px 2px 0px;
     border-color: gray;
+`;
+
+const CustomSelect = styled.select`
+    width: 24vw;
+    height: 60px;
+    border : none;
+    margin-left: 3vw;
+    font-size: 6vw;
+    /* @media screen and (max-width:1200px) { //반응형 웹 폰트 디자인
+        font-size: 4vw;
+    }
+    @media screen and (max-width:430px) {
+        font-size: 8vw;
+    } */
+
+    background-color: #ffffff;
+
+    -webkit-appearance: none;
+	-moz-appearance: none;
+	appearance: none;
+    background-image: url(${DownArrow});
+  background-repeat: no-repeat;
+  background-position: right center;
+  background-size: 7vw;
 `;
 
 const ContentContainer = styled.div`
@@ -60,21 +84,5 @@ const ContentContainer = styled.div`
     height: calc(100vh - 169px - 204px); /* 콘텐츠 영역 높이 설정 (헤더와 푸터 높이 뺀 값) */
 `;
 
-const CustomSelect = styled.select`
-    width: 230px;
-    height: 60px;
-    border : none;
-    margin: 42px;
-    font-size: 53px;
-    background-color: #ffffff;
-
-    -webkit-appearance: none;
-	-moz-appearance: none;
-	appearance: none;
-    background-image: url(${DownArrow}); /* 이미지 경로 설정 */
-  background-repeat: no-repeat; /* 이미지 반복 없음 */
-  background-position: right center; /* 이미지 위치 설정 */
-  background-size: 50px; /* 이미지 크기 조절 */
-`;
 
 export default MainPage;
--- src/pages/Myname.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Info = () => {
-    return(
-        <p>Pages에 만들어주세요</p>
-    )
-}
-
-export default Info;
